7f8f5d369365a0c2c5e4cbe5d1a25e2b13890bbf
Badr Essefiany <71149418+BqdrEss@users.noreply.github.com>
2022-11-25 15:48:52 +0100

    Update a2_5.c

--- a2/a2_5.c
+++ a2/a2_5.c
@@ -1,20 +1,15 @@
-/*
-CH-230-A
-a2_p5.c
-Badr Essefiany
-Bessefiany@jacobs-university.de
-*/
 #include <stdio.h>
+
 int main()
 {
     int a;
-        scanf("%d",&a);
-            printf("%d\n",a);
+    scanf("%d",&a);
+    printf("%d\n",a);
     int*ptr;
-        ptr=&a;
-            printf("the address is %u ",&a);
+    ptr=&a;
+    printf("the address is %p \n",&a);
     *ptr= *ptr+5;
-    printf("the value is %d ",a);
-    printf("the address is %d ",ptr);
+    printf("the value is %d \n",a);
+    printf("the address is %p \n",ptr);
 return 0;
 }
